b2443a969b6d0be78d36d78e9c50d91a9ed84114
ivan <ivan@ivan-VirtualBox.(none)>
2013-12-09 19:15:37 +0100

    Test sha1 update

--- test/testGenerator.c
+++ test/testGenerator.c
@@ -3,9 +3,11 @@
 #include <sys/types.h>
 #include <time.h>
 #include <string.h>
+#include <openssl/sha.h>
 #include "../devices/sha1.h"
 #include "../devices/reduction.h"
 
+// apt-get install libcurl4-openssl-dev
 
 const unsigned int REP = 1000000 ;
 
@@ -32,6 +34,16 @@ void test_sha1(SHA1Context *h){
     }
 }
 
+void test_sha1_openssl(){
+    unsigned char ibuf[] = "Password";
+    unsigned char obuf[20];
+
+    unsigned int i;
+    for (i = 0; i < REP; i++) {
+        SHA1(ibuf, strlen(ibuf), obuf);
+    }
+}
+
 void test_reduction(SHA1Context *h){
     char *r = (char *) malloc(10);
     unsigned int i, index;
@@ -52,13 +64,15 @@ char *argv[];
     clock_t t_ini2, t_fin2;
     double secs1, secs2;
 
+    reduction_init(7, "a0");
     SHA1Context h;
+
     t_ini1 = clock();     
         test_sha1(&h);
     t_fin1 = clock();
 
     t_ini2 = clock();
-        test_reduction(&h);
+        test_sha1_openssl();
     t_fin2 = clock();
 
     secs1 = (double)(t_fin1 - t_ini1) / CLOCKS_PER_SEC;
