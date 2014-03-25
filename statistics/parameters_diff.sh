e4e59594ba744190653a532f9079b4d1245d95cb
ivan <ivan.martin@11paths.com>
2014-03-25 17:47:12 +0100

    add statistics file for parameters

---
+++ statistics/parameters_diff.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# try to compare different executions with different values for parameters (tables_number, chain_lenghth, table_length)
+
+
+charset=a
+keylength=6
+threads=8
+
+# prueba 1. tables number
+table_length=50000
+chain_length=1000
+
+tables_number=1
+while [  $tables_number <= 5 ]
+do
+
+  let tables_number=tables_number+1
+done
+
+# prueba 2. chain length
+table_length=50000
+tables_number=5
+
+chain_length=200
+while [  $chain_length <= 1000 ]
+do
+
+  let chain_length=chain_length+200 
+done
+
+# prueba 3. table length
+chain_length=1000
+tables_number=5
+
+table_length=10000
+while [  $table_length <= 50000 ]
+do
+
+  let table_length=table_length+10000
+done
